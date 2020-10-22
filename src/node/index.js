d586f85fceb9aeb6892ac56968257f8a83cf3c7f
Marcus <mhagglun@gmail.com>
2020-10-22 10:42:01 +0200

    Implement graphs for orders and countries

--- src/node/index.js
+++ src/node/index.js
@@ -40,7 +40,12 @@ cassandraClient
     cassandraClient.execute(
       "CREATE TABLE IF NOT EXISTS access_log.orders (word text PRIMARY KEY, count float);"
     )
-  );
+  .then(() =>
+    cassandraClient.execute(
+      `CREATE TABLE IF NOT EXISTS access_log.countries (country_code text PRIMARY KEY, count float);`
+    )
+  )
+);
 
 io.sockets.on("connection", function (socket) {
   console.log("User connected");
@@ -49,6 +54,10 @@ io.sockets.on("connection", function (socket) {
   });
 });
 
+
+// TODO: Replace by reading data from output kafka topic instead,
+// Only load data directly from db when instantiating view
+
 // Continiously update chart.
 setInterval(function () {
   if (io.engine.clientsCount > 0) {
@@ -56,7 +65,33 @@ setInterval(function () {
       "SELECT * FROM access_log.searches;",
       (err, result) => {
         if (err) return;
-        io.emit("chart_data", result.rows);
+        io.emit("search_data", result.rows);
+      }
+    );
+  }
+}, 1000); //update every sec.
+
+// Continiously update chart.
+setInterval(function () {
+  if (io.engine.clientsCount > 0) {
+    cassandraClient.execute(
+      "SELECT * FROM access_log.orders;",
+      (err, result) => {
+        if (err) return;
+        io.emit("order_data", result.rows);
+      }
+    );
+  }
+}, 1000); //update every sec.
+
+// Continiously update chart.
+setInterval(function () {
+  if (io.engine.clientsCount > 0) {
+    cassandraClient.execute(
+      "SELECT * FROM access_log.countries;",
+      (err, result) => {
+        if (err) return;
+        io.emit("country_data", result.rows);
       }
     );
   }
